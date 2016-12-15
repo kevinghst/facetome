6247477941910fe282debbf710814259ae350729
kevinghst <wancongzhang@gmail.com>
2016-12-15 09:53:56 -0500

    reset

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module FaceBook
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_protocol => :https,
-      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
