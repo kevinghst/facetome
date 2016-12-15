5ef128b033ad41ca36bdc59d270d35f43f2bfe72
kevinghst <wancongzhang@gmail.com>
2016-12-15 16:48:22 -0500

    wrote postitem component

--- frontend/components/newsfeed/newsfeed.jsx
+++ frontend/components/newsfeed/newsfeed.jsx
@@ -1,82 +1,8 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 import Comments from '../comments';
+import PostItem from '../postitem';
 
-const PostItem = ({ post, deletePost, handler, displayDelete, currentUser, updateComment,
-                    submitComment, commentBody }) => {
-  let postImage = (
-    <div className="post-img">
-      <img src={post.image_url}/>
-    </div>
-  );
-
-  let nameLink;
-  if (post.target_id === post.author_id){
-    nameLink = (<Link className="post-name-link-real"
-                      to={`/home/${post.author.username}`}
-                      >{post.author.firstname} {post.author.lastname}
-                </Link>);
-  } else {
-    nameLink = (
-      <div className="authorToTarget">
-        <Link className="post-name-link-real"
-              to={`/home/${post.author.username}`}
-              >{post.author.firstname} {post.author.lastname}
-        </Link>
-        <img src={window.smalltriangle}/>
-        <Link className="post-name-link-real"
-              to={`/home/${post.target.username}`}
-              > {post.target.firstname} {post.target.lastname}
-        </Link>
-      </div>
-    );
-  }
-
-  let removePost;
-  if(currentUser && (currentUser.username === post.author.username || currentUser.username === post.target.username)){
-    removePost = (
-      <button className="deletePost" value={`${post.id}`} onClick={deletePost}>Delete</button>
-    );
-  }
-
-
-  return (
-    <li className="post-item">
-      <div className="post-content">
-        <div className="post-author-thumb">
-          <Link className="post-thumb-img" to={`/home/${post.author.username}`}>
-            <img src={post.author.photo_url}/>
-          </Link>
-          <div className="post-name-link">
-            {nameLink}
-            <div className="post-date">{post.date} at {post.time}</div>
-          </div>
-        </div>
-
-        <div className="post-text">
-          {post.body}
-        </div>
-
-        { (post.image_url.indexOf("/assets/monolith") === -1) && postImage }
-      </div>
-
-      <Comments updateComment={updateComment}
-                post={post}
-                currentUser={currentUser}
-                submitComment={submitComment}
-                commentBody={commentBody}
-        />
-
-      <div className="dropdown">
-        <a href='#'>
-          <img src={window.dropdown}/>
-          { removePost }
-        </a>
-      </div>
-
-    </li>
-  );
-};
 
 class NewsFeed extends React.Component{
   constructor(props){
@@ -240,7 +166,8 @@ class NewsFeed extends React.Component{
                                           currentUser={this.props.currentUser}
                                           updateComment={this.updateComment}
                                           submitComment={this.submitComment}
-                                          commentBody={this.state.commentBody} />)
+                                          commentBody={this.state.commentBody}
+                                          dynamicSet={this.dynamicSet} />)
             }
           </ul>
 
--- frontend/components/wall/wall.jsx
+++ frontend/components/wall/wall.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 
+
 const PostItem = ({ post, deletePost, handler, displayDelete, currentUser }) => {
   let postImage = (
     <div className="post-img">
