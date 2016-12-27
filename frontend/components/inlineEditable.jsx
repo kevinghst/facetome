cf1c0a262f80c896ba2dadd10e0328376489114e
kevinghst <wancongzhang@gmail.com>
2016-12-27 09:54:58 -0500

    finish inline editing

--- frontend/components/inlineEditable.jsx
+++ frontend/components/inlineEditable.jsx
@@ -25,26 +25,32 @@ class InlineEditable extends React.Component {
   }
 
   render(){
-    let profileValue = this.props.profileValue || " ";
+    let profileValue = this.props.profileValue;
+    if(profileValue==="null"){ profileValue=" " };
     let condition = true;
     if(this.props.currentUser && this.props.profile && (this.props.currentUser.id !== this.props.profile.id)){
       condition = false;
     }
     if (this.state.editing){
       return (
-        <div>{this.props.profileKey}</div>
-        <form className="profileform-double-edit group" onSubmit={ this.handleSubmit }>
-          <input className="profileform-input-edit" type="text" onChange= {this.updateForm}/>
-          <button className="profileform-button-edit" type="submit">Save Changes</button>
-        </form>
+        <div className="profile-edit group">
+          <div className="profile-keys">{this.props.profileKey}</div>
+          <form className="profileform-double-edit group" onSubmit={ this.handleSubmit }>
+            <input className="profileform-input-edit" type="text" onChange= {this.updateForm}/>
+            <button className="profileform-button-edit" type="submit">Save Changes</button>
+          </form>
+        </div>
+
       );
     } else {
       return (
-        <div>{this.props.profileKey}</div>
-        <section className="profileform-double-unedit group">
-          <p className="profileform-input-unedit">{profileValue}</p>
-          {condition && <button className="profileform-button-unedit" onClick={ ()=>this.setState({ editing: true})}>Edit</button>}
-        </section>
+        <div className="profile-unedit group">
+          <div className="profile-keys">{this.props.profileKey}</div>
+          <section className="profileform-double-unedit group">
+            <p className="profileform-input-unedit">{profileValue}</p>
+            {condition && <button className="profileform-button-unedit" onClick={ ()=>this.setState({ editing: true})}>Edit</button>}
+          </section>
+        </div>
       );
     }
   }
