cf7d1500d39791e95620caad307b989334325216
kevinghst <wancongzhang@gmail.com>
2017-01-02 11:34:40 -0500

    delete minification warning

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 const App = ({children}) => (
   <div>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
     {children}
   </div>
 );
--- frontend/facetome.jsx
+++ frontend/facetome.jsx
@@ -2,18 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
 import Root from './components/root';
-import {deleteFriend, createPostTest} from './util/friendship_api_util';
-import {createPost, fetchWall, deletePost} from './actions/post_actions';
-import {createComment, deleteComment} from './actions/comment_actions';
-import {createLike} from './util/like_api_util';
-import {fetchAllProfiles} from './actions/profile_actions';
-import {createMessage, fetchConvos} from './actions/convo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-
-  window.fetchAllProfiles = fetchAllProfiles;
-  window.createMessage = createMessage;
-  window.fetchConvos = fetchConvos;
 
   let store;
 
